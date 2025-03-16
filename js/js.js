593e1c965cfdac731f2d1f158c849549525b2bd9
lyuba3110 <lyuba.ustyugova@mail.ru>
2025-03-16 19:12:04 +0500

    fix: modal switcher

--- js/js.js
+++ js/js.js
@@ -102,3 +102,17 @@ const lightModeOff = (event) =>{
       prevEl: '.blog-button-prev',
     },
   });
+
+  const modal = document.querySelector(".modal");
+  const modalToggle = document.querySelectorAll('[data-toggle=modal]');
+  const modalClose = document.querySelector(".modal-close");
+  modalToggle.forEach((element) =>{
+    element.addEventListener("click", (event) =>{
+      event.preventDefault();
+      modal.classList.add("is-open")
+    });
+  });
+  modalClose.addEventListener("click", (event) =>{
+    event.preventDefault();
+    modal.classList.remove("is-open");
+  });
